c0def7b2995ff3932e6e84e4d54281aa3dabd9df
tinker49 <tinker7@protonmail.com>
2019-06-11 21:53:49 -0400

    changed defult_integer from 7 to 3

--- hello.rs
+++ hello.rs
@@ -9,7 +9,7 @@ fn main() {
 
     // Or a default will be used.
     let default_float   = 3.0; // `f64`
-    let default_integer = 7;   // `i32`
+    let default_integer = 3;   // `i32`
     
     // A type can also be inferred from context 
     let mut inferred_type = 12; // Type i64 is inferred from another line
